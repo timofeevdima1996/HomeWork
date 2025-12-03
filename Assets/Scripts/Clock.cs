e79ad372fb3a16476f155a7e29d9937e496533aa
Dmitrii <timofeevdima1996@gmail.com>
2025-12-03 08:43:09 +0300

    1.3

---
+++ Assets/Scripts/Clock.cs
@@ -0,0 +1,52 @@
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Clock : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI _text;
+    [SerializeField] private float _delay = 0.5f;
+    [SerializeField] private float _step = 1f;
+
+    private float _number = 0f;
+    private bool _isWork;
+    private Coroutine _counterCoroutine;
+
+    void Start()
+    {
+        _isWork = true;
+        _text.text = _number.ToString();
+        _counterCoroutine = StartCoroutine(Counter());
+    }
+
+    private IEnumerator Counter()
+    {
+        while (true)
+        {
+            if (_isWork)
+            {
+                _number += _step;
+                _text.text = _number.ToString();
+            }
+
+            yield return new WaitForSeconds(_delay);
+        }
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0) && Input.GetKeyDown(KeyCode.W))
+        {
+            _isWork = !_isWork;
+        }
+    }
+
+    void OnDestroy()
+    {
+        if (_counterCoroutine != null)
+        {
+            StopCoroutine(_counterCoroutine);
+        }
+    }
+}
